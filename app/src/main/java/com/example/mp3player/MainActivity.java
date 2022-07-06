478a1551e680f7d2bc617d831c332f23892667f1
RG-67 <rittikghoshkolkata@gmail.com>
2022-07-06 10:58:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mp3player/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.mp3player;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private SeekBar seekBar;
+    private MediaPlayer mediaPlayer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        seekBar = findViewById(R.id.seekBar);
+        mediaPlayer = MediaPlayer.create(this,R.raw.on);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mediaPlayer.isPlaying()) {
+                    mediaPlayer.pause();
+                    button.setText("Play");
+                }
+                else{
+                    mediaPlayer.start();
+                    button.setText("Pause");
+                }
+            }
+        });
+        seekBar.setMax(mediaPlayer.getDuration());
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(fromUser){
+                    mediaPlayer.seekTo(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
